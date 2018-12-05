6822e1c14c4b8094ab29bfe50efc046c3cd6e206
Stephen Coogan <hello@coog.ie>
2018-12-05 22:31:11 +0000

    fix: migrate to PureComponent

--- src/components/presentational/AlertMessage/AlertMessage.jsx
+++ src/components/presentational/AlertMessage/AlertMessage.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { PureComponent } from "react";
+import { bool, string } from "prop-types";
 import { Alert } from "antd";
 
-export const AlertMessage = props => {
-  const { isVisible, message } = props;
-  return isVisible ? <Alert message={message} showIcon /> : null;
-};
+export class AlertMessage extends PureComponent {
+  static propTypes = {
+    isVisible: bool,
+    message: string
+  };
+  static defaultProps = {
+    isVisible: false,
+    message: null
+  };
+
+  render() {
+    const { isVisible, message } = this.props;
+    return isVisible ? <Alert message={message} showIcon /> : null;
+  }
+}
--- src/components/presentational/SearchResults/SearchResults.jsx
+++ src/components/presentational/SearchResults/SearchResults.jsx
@@ -1,4 +1,5 @@
-import React, { Fragment } from "react";
+import React, { Fragment, PureComponent } from "react";
+import { arrayOf, bool, number, shape, string } from "prop-types";
 import { List, Avatar, Icon } from "antd";
 
 const ListItem = List.Item;
@@ -11,37 +12,65 @@ const IconText = ({ type, text }) => (
   </Fragment>
 );
 
-export const SearchResults = props => {
-  const { isLoading, results } = props;
+export class SearchResults extends PureComponent {
+  static propTypes = {
+    isLoading: bool,
+    results: arrayOf(
+      shape({
+        name: string,
+        html_url: string,
+        description: string,
+        updated_at: string,
+        homepage: string,
+        stargazers_count: number,
+        stargazers_count_formatted: string,
+        owner: shape({
+          login: string,
+          avatar_url: string,
+          html_url: string
+        })
+      })
+    ).isRequired
+  };
+  static defaultProps = {
+    isLoading: false
+  };
 
-  const header =
-    results.length > 0 ? `Showing ${results.length} results` : null;
+  render() {
+    const { isLoading, results } = this.props;
 
-  return (
-    <Fragment>
-      <List
-        header={header}
-        loading={isLoading}
-        itemLayout="vertical"
-        dataSource={results}
-        renderItem={item => (
-          <ListItem
-            actions={[
-              <IconText type="star-o" text={item.stargazers_count_formatted} />
-            ]}
-          >
-            <ListItemMeta
-              avatar={<Avatar src={item.owner.avatar_url} />}
-              title={
-                <a href={item.html_url} rel="noopener">
-                  {item.name} <small>({item.owner.login})</small>
-                </a>
-              }
-            />
-            {item.description}
-          </ListItem>
-        )}
-      />
-    </Fragment>
-  );
-};
+    const header =
+      results.length > 0 ? `Showing ${results.length} results` : null;
+
+    return (
+      <Fragment>
+        <List
+          header={header}
+          loading={isLoading}
+          itemLayout="vertical"
+          dataSource={results}
+          renderItem={item => (
+            <ListItem
+              actions={[
+                <IconText
+                  type="star-o"
+                  text={item.stargazers_count_formatted}
+                />
+              ]}
+            >
+              <ListItemMeta
+                avatar={<Avatar src={item.owner.avatar_url} />}
+                title={
+                  <a href={item.html_url} rel="noopener">
+                    {item.name} <small>({item.owner.login})</small>
+                  </a>
+                }
+              />
+              {item.description}
+            </ListItem>
+          )}
+        />
+      </Fragment>
+    );
+  }
+}
